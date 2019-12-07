5846a18a1631663f1a5d36f920cf40901ed13ae4
Andrew Berry <andrew.berry@outlook.com>
2019-12-08 08:19:37 +1000

    register service worker

--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import { App } from "./App";
 
 import { useGameState } from "../../hooks/useGameState";
-import { generatePuzzle } from "../../utils/generatePuzzle";
 
 export function AppContainer() {
   const {
