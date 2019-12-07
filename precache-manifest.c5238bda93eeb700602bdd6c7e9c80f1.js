1dc5bdf73bbecced964f0e6ef549ab4c981ce484
Andrew Berry <andrew.berry@outlook.com>
2019-12-08 08:36:03 +1000

    Updates

---
+++ precache-manifest.c5238bda93eeb700602bdd6c7e9c80f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c03d1476b6731b1f46023156d0faa48c",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "758ef0f4456fd02f44f6",
+    "url": "/sudoku/static/css/main.c8b44197.chunk.css"
+  },
+  {
+    "revision": "7c575e358a9152d6394f",
+    "url": "/sudoku/static/js/2.4b8922bc.chunk.js"
+  },
+  {
+    "revision": "758ef0f4456fd02f44f6",
+    "url": "/sudoku/static/js/main.7fc51b93.chunk.js"
+  },
+  {
+    "revision": "60a5e5439fa8d06f9652",
+    "url": "/sudoku/static/js/runtime-main.6c2b2ca1.js"
+  }
+]);
